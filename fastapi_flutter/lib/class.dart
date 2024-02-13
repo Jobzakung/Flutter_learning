2dadb5f17f1de2651d9a1be47b5bba3f15b063d2
jobzakung <jobzakung@naver.com>
2024-02-13 20:02:45 +0700

    can't Post user

--- fastapi_flutter/lib/class.dart
+++ fastapi_flutter/lib/class.dart
@@ -1,16 +1,183 @@
-class RestApi {
-  final int id;
-  final String name;
+import 'package:flutter/material.dart';
 
-  const RestApi({
-    required this.id,
-    required this.name,
+class Restapi {
+  List<Users>? users;
+
+  Restapi({this.users});
+
+  Restapi.fromJson(Map<String, dynamic> json) {
+    if (json['users'] != null) {
+      users = <Users>[];
+      json['users'].forEach((v) {
+        users!.add(new Users.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.users != null) {
+      data['users'] = this.users!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Users {
+  int? id;
+  String? name;
+
+  Users({this.id, this.name});
+
+  Users.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    return data;
+  }
+}
+
+class UserCard extends StatelessWidget {
+  final Users user;
+  final Function(Users)? onUpdate;
+  final Function(int)? onDelete;
+  final Function(String)? onAddUser;
+
+  UserCard({
+    required this.user,
+    required this.onUpdate,
+    required this.onDelete,
+    required this.onAddUser,
   });
 
-  factory RestApi.fromJson(Map<String, dynamic> json) {
-    return RestApi(
-      id: json['id'],
-      name: json['name'],
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      ListTile(
+        leading: const Icon(Icons.account_circle, color: Colors.blue),
+        title: Text('${user.name}'),
+        trailing: SizedBox(
+          width: 100,
+          child: Row(children: [
+            IconButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Edit User'),
+                        content: TextField(
+                          controller: TextEditingController(text: user.name),
+                          onChanged: (value) {
+                            user.name = value;
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              onUpdate!(user);
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Save'),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              icon: Icon(Icons.edit, color: Colors.yellow),
+            ),
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Confirm Delete'),
+                      content:
+                          Text('Are you sure you want to delete this user?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            onDelete!(user.id!);
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Delete'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              icon: Icon(Icons.delete, color: Colors.red),
+            ),
+          ]),
+        ),
+      )
+    ]);
+  }
+}
+
+class AddUserDialog extends StatefulWidget {
+  final Function(String) onAddUser;
+
+  AddUserDialog({required this.onAddUser});
+
+  @override
+  _AddUserDialogState createState() => _AddUserDialogState();
+}
+
+class _AddUserDialogState extends State<AddUserDialog> {
+  final _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Add User'),
+      content: TextField(
+        controller: _nameController,
+        decoration: InputDecoration(labelText: 'Name'),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            final String name = _nameController.text.trim();
+            if (name.isNotEmpty) {
+              widget.onAddUser(name);
+              Navigator.of(context).pop();
+            }
+          },
+          child: Text('Add'),
+        ),
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
   }
 }
--- fastapi_flutter/lib/hosting.dart
+++ fastapi_flutter/lib/hosting.dart
@@ -1,259 +1,118 @@
 import 'dart:convert';
-
-import 'package:fastapi_flutter/class.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'class.dart';
 
 class Hosting extends StatefulWidget {
-  const Hosting({Key? key}) : super(key: key);
+  const Hosting({super.key});
 
   @override
   State<Hosting> createState() => _HostingState();
 }
 
 class _HostingState extends State<Hosting> {
-  dynamic data = "";
+  List<Users>? users = [];
 
-  TextEditingController _nameController =
-      TextEditingController(); // Moved outside the method
+  Future<void> fetchUsers() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:8000/users'));
+    if (response.statusCode == 200) {
+      setState(() {
+        users = Restapi.fromJson(jsonDecode(response.body)).users;
+      });
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  Future<void> insertUser(String name) async {
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8000/users'),
+      body: jsonEncode({'name': name}),
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": "application/json"
+      },
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        fetchUsers();
+      }); // Refresh user list
+    } else {
+      throw Exception('Failed to insert user');
+    }
+  }
+
+  Future<void> updateUser(Users user) async {
+    final response = await http.put(
+      Uri.parse('http://10.0.2.2:8000/users/${user.id}'),
+      body: jsonEncode(user.toJson()),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        users!.forEach((element) {
+          if (element.id == user.id) {
+            element.name = user.name;
+          }
+        });
+      });
+      // User updated successfully
+    } else {
+      throw Exception('Failed to update user');
+    }
+  }
+
+  Future<void> deleteUser(int userId) async {
+    final response =
+        await http.delete(Uri.parse('http://10.0.2.2:8000/users/$userId'));
+    if (response.statusCode == 200) {
+      setState(() {
+        users!.removeWhere((user) => user.id == userId);
+      });
+    } else {
+      throw Exception('Failed to delete user');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hosting'),
+        title: Text('User List'),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: Column(
-        children: [
-          Text("$data"),
-          ElevatedButton(
-            onPressed: () async {
-              await fetchData(); // Await fetchData to ensure the UI updates properly
-            },
-            child: const Text("Fetch Data"),
-          ),
-          data.isEmpty
-              ? const Text("No data")
-              : SingleChildScrollView(
-                  child: SizedBox(
-                    height: 300,
-                    child: toList(),
-                  ),
-                ),
-          if (data == null) // Show a message if data is null
-            const Text('No data available'),
-        ],
-      ),
+      body: users == null
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: users!.length,
+              itemBuilder: (context, index) {
+                return UserCard(
+                  user: users![index],
+                  onUpdate: updateUser,
+                  onDelete: deleteUser,
+                  onAddUser: insertUser,
+                );
+              },
+            ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => insertData(context),
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  Future<RestApi> fetchData() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:8000/users'));
-    if (response.statusCode == 200) {
-      final jsonData = jsonDecode(response.body);
-      data = jsonData;
-      setState(() {});
-      return RestApi.fromJson(
-          jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      throw Exception('failed to load json');
-    }
-  }
-  // Future<void> fetchData() async {
-  //   try {
-  //     var url = Uri.parse('http://10.0.2.2:8000/users');
-  //     final response = await http.get(url);
-  //     if (response.statusCode == 200) {
-  //       setState(() {
-  //         data = jsonDecode(response.body);
-  //       });
-  //     } else {
-  //       throw Exception('Failed to connect');
-  //     }
-  //   } catch (e) {
-  //     print('Error fetching data: $e');
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(content: Text('Failed to fetch data')),
-  //     );
-  //   }
-  // }
-
-  void insertData(BuildContext context) {
-    showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Register New User"),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: [
-                TextField(
-                  controller: _nameController,
-                ),
-                const Text("Enter the name of the user"),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                addData(_nameController.text);
-                Navigator.of(context).pop();
-              },
-              child: const Text("Confirm"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void UpdateData(BuildContext context) {
-    showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Update User"),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: [
-                TextField(
-                  controller: _nameController,
-                ),
-                const Text("Enter the new name of the user"),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                add_edit(_nameController.text, data['id']);
-                Navigator.of(context).pop();
-              },
-              child: const Text("Confirm"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Widget toList() {
-    if (data == null || data.isEmpty) {
-      return const Text('No data available');
-    } else {
-      return ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: ListTile(
-              leading: const Icon(
-                Icons.account_circle,
-                color: Colors.red,
-              ),
-              title: Text("${data[index]?['name'] ?? 'Unknown'}"),
-              trailing: SizedBox(
-                width: 100,
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        UpdateData(context);
-                      },
-                      icon: const Icon(
-                        Icons.edit,
-                        color: Colors.green,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        delete(data[index]['id']);
-                      },
-                      icon: const Icon(Icons.delete, color: Colors.red),
-                    )
-                  ],
-                ),
-              ),
-            ),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AddUserDialog(
+                onAddUser: insertUser,
+              );
+            },
           );
         },
-      );
-    }
-  }
-
-  Future<void> addData(String name) async {
-    try {
-      Map<String, dynamic> userData = {'name': name};
-      var body = jsonEncode(userData);
-      var response = await http.post(
-        Uri.http('10.0.2.2:8000', '/users'),
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: body,
-      );
-      if (response.statusCode == 200) {
-        await fetchData();
-      } else {
-        throw Exception('Failed to add data');
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Failed to add data')),
-      );
-    }
-  }
-
-  Future<void> add_edit(String userId, String newName) async {
-    try {
-      Map<String, dynamic> userData = {'name': newName};
-      var body = jsonEncode(userData);
-      var response = await http.put(
-        Uri.http('10.0.2.2:8000', '/users/$userId'),
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: body,
-      );
-      if (response.statusCode == 200) {
-        await fetchData();
-      } else {
-        throw Exception('Failed to update user');
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Failed to update user')),
-      );
-    }
-  }
-
-  Future<void> delete(String userId) async {
-    try {
-      // Send a DELETE request to remove the user
-      var url = Uri.parse('http://10.0.2.2:8000/users/$userId');
-      var response = await http.delete(
-        url,
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-      );
-      if (response.statusCode == 200) {
-        // Refresh data after successful deletion
-        await fetchData();
-      } else {
-        throw Exception('Failed to delete user');
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Failed to delete user')),
-      );
-    }
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
